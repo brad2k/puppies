d871cf4377ccf4b394fe1f5f62fe52d460b42ad7
Brad Azevedo <brad2k@gmail.com>
2019-02-13 20:28:01 -0800

    Minor cleanup

--- src/css-bootstrap.js
+++ src/css-bootstrap.js
@@ -8,30 +8,30 @@ export const theme = {
 };
 
 export const GlobalStyles = createGlobalStyle`
-    html {
-        box-sizing: border-box;
-        font-size: 10px;
-        min-height: 100vh;
-    }
-  
-    *, *:before, *:after {
-        box-sizing: inherit;
-    }
-  
-    body {
-        font-size: 1.6rem;
-        line-height: 1.8;
-        padding: 0;
-        margin: 0;
-        color: ${props => props.theme.text};
-        font-family: 'Montserrat', sans-serif;
-  
-        &.disable-scroll {
-          overflow: hidden;
-        }
-    }
-  
-    p {
+  html {
+      box-sizing: border-box;
+      font-size: 10px;
+      min-height: 100vh;
+  }
+
+  *, *:before, *:after {
+      box-sizing: inherit;
+  }
+
+  body {
+      font-size: 1.6rem;
+      line-height: 1.8;
+      padding: 0;
       margin: 0;
-    }
-  `;
+      color: ${props => props.theme.text};
+      font-family: 'Montserrat', sans-serif;
+
+      &.disable-scroll {
+        overflow: hidden;
+      }
+  }
+
+  p {
+    margin: 0;
+  }
+`;
